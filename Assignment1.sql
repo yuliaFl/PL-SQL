97a28a34484f52d88bb6f464b764a4e13ed0565a
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-10-25 14:47:21 -0400

    complete question 1, assignment 1

--- Assignment1.sql
+++ Assignment1.sql
@@ -10,68 +10,43 @@ The values inserted into the temp table show that the two x 's are indeed differ
 You will have to create the table temp as follows:
 */
 
+CREATE TABLE TEMP(
+NUM_ONE NUMBER(5),
+NUM_TWO NUMBER(2),
+CHAR_OUT VARCHAR2(30));
 
+-- Outer block 
+<<outer>> 
 DECLARE
--- variables declared
-    X NUMBER := 0; 
-    V_COUNTER NUMBER := 0;
+    -- required variables
+    x NUMBER := 0;
+    counter NUMBER := 4;
 BEGIN
-    LOOP -- first loop 
-        V_COUNTER := V_COUNTER + 1;
-            IF V_COUNTER > 4 THEN
-            EXIT;
-            END IF;
-        LOOP
-            V_COUNTER := V_COUNTER + 1;
-            IF V_COUNTER > 20 THEN
-            EXIT;
-            END IF;
-        INSERT INTO TEMP
-        VALUES (X, V_COUNTER, 'in INNER loop');
-      END LOOP;
+-- first loop for the thousands numbers
+    FOR i IN 1..counter LOOP
+    -- increase x (work with coloumn 2)
+        x := x + 1;
+        INSERT INTO TEMP (NUM_ONE, NUM_TWO, CHAR_OUT) VALUES (i*1000, x, 'in OUTER loop'); -- thousands increase inside the insert
+        --inner block 
+        DECLARE 
+        -- required variables
+            x NUMBER := 0;
+            counter NUMBER := 4;
+        BEGIN 
+        -- second for loop
+            FOR j IN 1..counter LOOP
+            -- continue to incease the second coloumn value
+            x := x + 1;
+            -- refer to the outer value
+            outer.x:= outer.x + 1;
+            INSERT INTO TEMP (NUM_ONE, NUM_TWO, CHAR_OUT) VALUES (x, outer.x, 'in INNER loop');
+            -- end inner loop
+            END LOOP;
+        -- end second block
+        END;
+    -- end first loop
     END LOOP;
 END;
-
 select * from temp;
-
 DELETE TEMP;
--- QUESTION 2
-/*
-A PL/SQL is needed to calculate annual raises and update employee salary amounts recorded in the EMP table. Create a block that addresses the requirements below. Note that all salaries in EMP table are recorded as monthly amounts. Display the calculated salaries for verification before including the update action.
-•	Calculate 5% annual raises for all employees except AD_VP.
-•	If the 5% totals to more than 2000, cap the raise to 2000.
-•	Update the salary for each employee in the table.
-•	For each employee display the current annual salary, raise, and proposed new annual salary.
-•	At the end, show the total amount of salary increase.
-
-NOTE: Create the table EMP as a copy of Employee table as follow:
-
-Create table emp (empno, ename, sal,job)
-As( select employee_id,first_name||' '||last_name,salary,job_id from employees)
-*/
-
--- QUESTION 3
-/*
-Use control Structure to write a program to display the language spoken by natives of the country. 
-Input the name of the country and display the language spoken by the natives of the country. 
-You can use any structure of your choice.
-*/
-
--- QUESTION 4
-/*
-Write a PL/SQL program (Using Cursors) which calculates the number of years and employee has worked in the company.
-Display this information only for the employees who are working in the Sales department. Your output should be similar to following.
-*/
-
--- QUESTION 5
-/*
-Write a PL/SQL program (Using Cursors) to list all the Managers from the employee’s table with list of the Employees reporting to the Manager. Both managers and Employees reporting to the managers should be listed in alphabetical order.Output should list following information 
-For manager:
-1.	First name
-2.	Last name 
-3.	Department name
-Employees reporting to Manager 
-1.	First name
-2.	Last name 
-3.	Department name
-*/
+DROP TABLE TEMP;
